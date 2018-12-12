dd56cab7af0ab5c776e4ac9fd9db5c832a56fdb3
SeanKrisp89 <SeanKrispin@gmail.com>
2018-12-11 22:05:20 -0600

    Add project files.

---
+++ List Exercise 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace List_Exercise_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
